61a6c6fe0ea29743c8450eb437eef6db93ea3ed4
Lara Komosa <larakomosa@gmail.com>
2020-11-22 15:39:10 -0600

    initial setup

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -20,11 +20,12 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 router.post('/register', (req, res, next) => {
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
+  const page_role_id = req.body.page_role_id;
 
-  const queryText = `INSERT INTO "user" (username, password)
-    VALUES ($1, $2) RETURNING id`;
+  const queryText = `INSERT INTO "user" (username, password, page_role_id)
+    VALUES ($1, $2, $3) RETURNING id`;
   pool
-    .query(queryText, [username, password])
+    .query(queryText, [username, password, page_role_id])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('User registration failed: ', err);
