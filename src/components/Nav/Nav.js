8aac1066d32e205ca6ccd4c974ea31427b778082
Lara Komosa <larakomosa@gmail.com>
2020-11-24 09:01:33 -0600

    determined correct rendering for nav bar with 2 user interfaces

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -12,14 +12,15 @@ const Nav = (props) => {
   };
 
   if (props.store.user.id != null) {
-    loginLinkData.path = '/user';
+    loginLinkData.path = '/feeling';
     loginLinkData.text = 'Home';
   }
 
+  if (props.store.user.page_role_id === 4) {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+        <h2 className="nav-title">Child</h2>
       </Link>
       <div className="nav-right">
         <Link className="nav-link" to={loginLinkData.path}>
@@ -44,6 +45,32 @@ const Nav = (props) => {
       </div>
     </div>
   );
-};
+        }else{  return (
+    <div className="nav">
+      <Link to="/home">
+        <h2 className="nav-title">Adult</h2>
+      </Link>
+      <div className="nav-right">
+        <Link className="nav-link" to={loginLinkData.path}>
+          {/* Show this link if they are logged in or not,
+          but call this link 'Home' if they are logged in,
+          and call this link 'Login / Register' if they are not */}
+          {loginLinkData.text}
+        </Link>
+        {/* Show the link to the info page and the logout button if the user is logged in */}
+        {props.store.user.id && (
+          <>
+            <Link className="nav-link" to="/info">
+              Info Page
+            </Link>
+            <LogOutButton className="nav-link" />
+          </>
+        )}
+        {/* Always show this link since the about page is not protected */}
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+      </div>
+    </div>)}}
 
 export default connect(mapStoreToProps)(Nav);
