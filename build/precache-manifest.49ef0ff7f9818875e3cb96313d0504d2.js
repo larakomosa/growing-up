49f51a3d2bd98f73925c4483929f6c41f2ff5d19
Lara Komosa <larakomosa@gmail.com>
2020-12-07 16:29:37 -0600

    production build for client

---
+++ build/precache-manifest.49ef0ff7f9818875e3cb96313d0504d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85eb7112ad7c2c2259d83baa65062a18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17eb9c2ae4146b7cfe38",
+    "url": "/static/css/main.0b64f156.chunk.css"
+  },
+  {
+    "revision": "376b20b63418fd051e4b",
+    "url": "/static/js/2.e9d74341.chunk.js"
+  },
+  {
+    "revision": "6bdf6becedb14e4609964580f4c3faf6",
+    "url": "/static/js/2.e9d74341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17eb9c2ae4146b7cfe38",
+    "url": "/static/js/main.117bc046.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  },
+  {
+    "revision": "770eadc7857f0461c26048aa0eaafdfe",
+    "url": "/static/media/3.9375494a.png"
+  },
+  {
+    "revision": "1b4667fb2b11d23fb4b4554d4401ce0b",
+    "url": "/static/media/7.99476a2f.jpg"
+  },
+  {
+    "revision": "0d1d4d66bd54819c3dea9624611516f7",
+    "url": "/static/media/background.56336a84.jpg"
+  },
+  {
+    "revision": "b41ac09e24e57c6724d9c292cd008bd5",
+    "url": "/static/media/final.d3629176.png"
+  },
+  {
+    "revision": "4de2d2cfcd9b18f0b064adeb7bccace8",
+    "url": "/static/media/header6.d3f26177.png"
+  }
+]);
